a101efef052cbf99b94607196ec46e3aec3304b5
Mary Alice Orr <m.a.orr17@gmail.com>
2018-04-16 19:16:09 -0500

    added items final

--- javascripts/departmentDom.js
+++ javascripts/departmentDom.js
@@ -3,7 +3,6 @@ const departmentOutputDiv = document.getElementById('departments');
 
 const cardBuilder = (departmentArray) => {
   let domString = '';
-  console.log('department-array', departmentArray);
   departmentArray.forEach((department) => {
     domString += `<div class='col-sm-3 department'>`;
     domString +=   `<h3 class='hide department-title' data-department-id='${department.id}'>${department.name}</h3>`;
