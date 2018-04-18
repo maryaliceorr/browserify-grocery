f0ba8288a917a1fb93f3554e349192b3063225c8
Mary Alice Orr <m.a.orr17@gmail.com>
2018-04-17 19:20:21 -0500

    added buttons

--- javascripts/buttonEvents.js
+++ javascripts/buttonEvents.js
@@ -1,9 +1,19 @@
 const dataGatekeeper = require('./dataGatekeeper');
 
 const goToDepartments = document.getElementById('go-to-departments');
+const goToCart = document.getElementById('go-to-cart');
+
 const departmentsDiv = document.getElementById('departments');
 const itemsDiv =  document.getElementById('items');
 const cartDiv = document.getElementById('cart');
+
+const cartButton = () => {
+  goToCart.addEventListener('click', () => {
+    departmentsDiv.innerHTML = '';
+    itemsDiv.innerHTML = '';
+    cartDiv.innerHTML = '';
+  });
+};
 
 const departmentsButton = () => {
   goToDepartments.addEventListener('click', () => {
@@ -16,4 +26,5 @@ const departmentsButton = () => {
 
 module.exports = {
   departmentsButton,
+  cartButton,
 };
